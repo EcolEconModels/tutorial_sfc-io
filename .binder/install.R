9ea078936f6e0d3e1135e699d9902861543463e6
Aditya Gilra <aditya_gilra@yahoo.com>
2026-02-18 18:02:04 +0100

    added knitr and rmarkdown packages for github actions. possible fixes for slowdown in .qmd run, but not fixed really.

--- .binder/install.R
+++ .binder/install.R
@@ -3,5 +3,5 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
 required_packages <- c("jsonlite", "ggplot2")
 missing_packages <- required_packages[!vapply(required_packages, requireNamespace, logical(1), quietly = TRUE)]
 if (length(missing_packages) > 0) {
-  install.packages(missing_packages)
+  install.packages(missing_packages, Ncpus = max(1L, parallel::detectCores() - 1L))
 }
