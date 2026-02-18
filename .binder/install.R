97a5ab538b3ebbf8c1c03c12b624cfbae2660cca
Aditya Gilra <aditya_gilra@yahoo.com>
2026-02-18 13:44:45 +0100

    also removed knitr from the installed packages list install.R for binder.

--- .binder/install.R
+++ .binder/install.R
@@ -1,6 +1,6 @@
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
-required_packages <- c("jsonlite", "ggplot2", "knitr")
+required_packages <- c("jsonlite", "ggplot2")
 missing_packages <- required_packages[!vapply(required_packages, requireNamespace, logical(1), quietly = TRUE)]
 if (length(missing_packages) > 0) {
   install.packages(missing_packages)
